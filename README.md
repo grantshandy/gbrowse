47dd400f16d1b79f737cbf30b032bbe6bf8b57ef
grantshandy <grantshandy@gmail.com>
2022-10-01 15:13:00 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# gbrowse (gemini browser)
+
+[`gemini project site`](https://gemini.circumlunar.space/)
+
+Personally I think the gemini project itself is kind of a gimmick, but some friends were talking about a browser and I decided to make this.
+
+It's not a masterpiece but I think it works quite well for the amount of time I put into it (~3hrs) and the number of SLOC (~200).
+
+[screenshot](./screenshots/screenshot.png)
+
+## build
+```
+$ cargo build
+```
+
+## run
+```
+$ cargo run
+```
---
+++ src/main.rs
@@ -0,0 +1,277 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
+
+use eframe::{
+    egui::{self, containers::Frame, output::OpenUrl, style::Margin, RichText, ScrollArea},
+    epaint::Color32,
+    NativeOptions, Renderer,
+};
+use gmi::{
+    gemtext::{self, GemtextNode},
+    protocol::StatusCode,
+    request,
+    url::Url,
+};
+
+use std::{
+    path::PathBuf,
+    str,
+    sync::{
+        mpsc::{self, Receiver, Sender, TryRecvError},
+        Arc,
+    },
+    thread,
+};
+
+fn main() {
+    let mut options = NativeOptions::default();
+
+    options.renderer = Renderer::Wgpu;
+
+    eframe::run_native(
+        "gbrowse",
+        options,
+        Box::new(|_cc| Box::new(Gbrowse::default())),
+    );
+}
+
+struct Gbrowse {
+    tx: Sender<Result<Vec<GemtextNode>, String>>,
+    rx: Receiver<Result<Vec<GemtextNode>, String>>,
+    sites: Vec<String>,
+    content: Option<Vec<GemtextNode>>,
+    error: Option<String>,
+    loading: bool,
+    url: String,
+}
+
+impl Default for Gbrowse {
+    fn default() -> Self {
+        let (tx, rx) = mpsc::channel();
+
+        let starting_page = "gemini://gemini.circumlunar.space";
+
+        Self {
+            tx,
+            rx,
+            sites: vec![],
+            content: None,
+            error: None,
+            loading: false,
+            url: starting_page.to_string(),
+        }
+    }
+}
+
+impl Gbrowse {
+    pub fn change_site(&mut self, url: &str, moving_back: bool) {
+        self.error = None;
+        self.content = None;
+
+        println!("going to {url}");
+
+        self.url = url.to_string();
+
+        let url_structured = match Url::try_from(url) {
+            Ok(url_structured) => url_structured,
+            Err(err) => {
+                self.error = Some(format!("Incorrectly formatted url: {err}"));
+
+                return;
+            }
+        };
+
+        let url = Arc::new(url_structured);
+        let tx = self.tx.clone();
+
+        if !moving_back {
+            self.sites.push(self.url.clone());
+        }
+
+        self.loading = true;
+
+        thread::spawn(move || {
+            tx.send(make_request(&url)).unwrap();
+        });
+    }
+
+    pub fn get_content(&mut self) -> Option<Vec<GemtextNode>> {
+        match self.rx.try_recv() {
+            Ok(content) => match content {
+                Ok(content) => {
+                    self.loading = false;
+                    Some(content)
+                }
+                Err(err) => {
+                    self.error = Some(err);
+                    self.loading = false;
+
+                    None
+                }
+            },
+            Err(err) => {
+                if err != TryRecvError::Empty {
+                    self.error = Some(format!("Error Recieving From Other Thread: {err}"));
+                    self.loading = false;
+                }
+
+                None
+            }
+        }
+    }
+}
+
+impl eframe::App for Gbrowse {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // get content back from other thread
+            if let Some(content) = self.get_content() {
+                self.content = Some(content);
+            }
+
+            // search bar
+            ScrollArea::horizontal()
+                .id_source("horizontal scroll")
+                .show(ui, |ui| {
+                    ui.horizontal(|ui| {
+                        if self.sites.len() > 1 {
+                            if ui.button("⏪").clicked() {
+                                self.sites.pop();
+                                if let Some(before) = self.sites.clone().last() {
+                                    self.change_site(before, true);
+                                }
+                            }
+                        }
+
+                        ui.text_edit_singleline(&mut self.url);
+
+                        if ui.button("🚀").clicked() {
+                            self.change_site(&self.url.clone(), false);
+                        }
+
+                        if self.loading {
+                            ui.label("loading...");
+                        }
+                    });
+                });
+
+            ui.separator();
+
+            // display error
+            if let Some(err) = &self.error {
+                ui.label(RichText::new(err).color(Color32::RED).strong());
+            }
+
+            // display text
+            if let Some(content) = &self.content.clone() {
+                ScrollArea::vertical()
+                    .id_source("vertical scroll")
+                    .auto_shrink([false, false])
+                    .show(ui, |ui| {
+                        for block in content {
+                            match block {
+                                GemtextNode::Text(text) => {
+                                    ui.label(text);
+                                }
+                                GemtextNode::Link(url, label) => {
+                                    let link = ui
+                                        .link(label.as_ref().unwrap_or(&url))
+                                        .on_hover_text_at_pointer(url);
+
+                                    if link.clicked() {
+                                        // if full url
+                                        if let Ok(parsed_url) = url::Url::parse(url) {
+                                            if parsed_url.scheme() == "http"
+                                                || parsed_url.scheme() == "https"
+                                            {
+                                                ui.ctx().output().open_url =
+                                                    Some(OpenUrl::new_tab(url));
+                                            // if gemini link
+                                            } else if parsed_url.scheme() == "gemini" {
+                                                self.change_site(url.as_str(), false);
+                                            }
+                                        } else {
+                                            // if relative url
+                                            let mut current_url =
+                                                url::Url::parse(&self.url.clone()).unwrap();
+                                            let mut current_path =
+                                                PathBuf::from(current_url.path());
+
+                                            let url_addition_path = PathBuf::from(url.clone());
+
+                                            if url_addition_path.is_absolute() {
+                                                current_path = url_addition_path;
+                                            } else {
+                                                current_path.push(url_addition_path);
+                                            }
+
+                                            current_url.set_path(
+                                                current_path.to_str().unwrap_or_default(),
+                                            );
+                                            self.change_site(current_url.as_str(), false);
+                                        }
+                                    }
+                                }
+                                GemtextNode::Heading(text) => {
+                                    ui.label(RichText::new(text).size(30.0));
+                                }
+                                GemtextNode::SubHeading(text) => {
+                                    ui.label(RichText::new(text).size(25.0));
+                                }
+                                GemtextNode::SubSubHeading(text) => {
+                                    ui.label(RichText::new(text).size(20.0));
+                                }
+                                GemtextNode::ListItem(text) => {
+                                    ui.label(format!("  • {text}"));
+                                }
+                                GemtextNode::Blockquote(text) => {
+                                    let frame = Frame {
+                                        outer_margin: Margin {
+                                            left: 15.0,
+                                            ..Margin::default()
+                                        },
+                                        ..Frame::default()
+                                    };
+
+                                    frame.show(ui, |ui| {
+                                        ui.label(text);
+                                    });
+                                }
+                                GemtextNode::Preformatted(text, _) => {
+                                    ui.code(text);
+                                }
+                                GemtextNode::EmptyLine => {
+                                    ui.add_space(10.0);
+                                }
+                            };
+                        }
+                    });
+            }
+        });
+    }
+}
+
+fn make_request(url: &Url) -> Result<Vec<GemtextNode>, String> {
+    let mut url = url.clone();
+
+    let data: Vec<u8> = loop {
+        let response = match request::make_request(&url) {
+            Ok(response) => response,
+            Err(err) => return Err(format!("Request Error: {err}")),
+        };
+
+        match response.status {
+            StatusCode::Redirect(_) => url = Url::try_from(response.meta.as_str()).unwrap(),
+            StatusCode::Success(_) => break response.data,
+            s => return Err(format!("Error: unknown status code: {:?}", s)),
+        }
+    };
+
+    let text = match str::from_utf8(&data) {
+        Ok(text) => text,
+        Err(err) => return Err(format!("Text Formatting Error: {err}")),
+    };
+
+    let gemtext = gemtext::parse_gemtext(text);
+
+    Ok(gemtext)
+}
